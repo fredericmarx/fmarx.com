1fff0a77963dbccb82ed61b140841c360c41e2a3
Frederic Marx <marx.frederic@gmail.com>
2016-05-20 19:47:40 +0200

    Add Google author tags

--- theme/scripts/sw.js
+++ theme/scripts/sw.js
@@ -1,10 +1,12 @@
-const cacheName = 'v1::static'
+const fontLocation = 'https://fmarx.com/fonts/'
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
+    caches.open('fonts').then(cache => {
       return cache.addAll([
-        ''
+        fontLocation + 'nittigrotesk-bold-v20.woff2',
+        fontLocation + 'nittigrotesk-normal-v20.woff2',
+        fontLocation + 'nittigrotesk-normalitalic-v20.woff2'
       ]).then(() => self.skipWaiting())
     })
   )
