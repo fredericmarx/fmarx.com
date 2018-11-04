449326478627dc0f3f0dca2ce98da9d7bffb9069
Frederic Marx <marx.frederic@gmail.com>
2018-11-04 13:38:49 +0100

    Add date to posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,27 @@
-module.exports = {
-	dir: {
-		input: 'src',
-		output: 'dist'
-  },
-  templateFormats: [
-    'md',
-    'njk',
-    'png',
-    'jpg',
-    'woff',
-    'woff2'
-  ],
-  passthroughFileCopy: true
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addNunjucksFilter('formatDate', date => {
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    }
+
+    return new Date(date).toLocaleDateString('en-US', options)
+  });
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist'
+    },
+    templateFormats: [
+      'md',
+      'njk',
+      'png',
+      'jpg',
+      'woff',
+      'woff2'
+    ],
+    passthroughFileCopy: true
+  }
 }
