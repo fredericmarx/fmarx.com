137e8b358c2ba09423b4fde04510a63d5122c136
Frederic Marx <marx.frederic@gmail.com>
2020-10-10 16:55:09 +0200

    Minify CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,31 @@
+const CleanCSS = require("clean-css");
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addNunjucksFilter('formatDate', date => {
+  eleventyConfig.addNunjucksFilter("formatDate", (date) => {
     const options = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    }
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
 
-    return new Date(date).toLocaleDateString('en-US', options)
-  })
+    return new Date(date).toLocaleDateString("en-US", options);
+  });
 
-  eleventyConfig.addCollection('til', function (collection) {
-    const glob = 'src/til/**/*.md'
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addCollection("til", function (collection) {
+    const glob = "src/til/**/*.md";
     return collection.getFilteredByGlob(glob);
-  })
+  });
 
   return {
     dir: {
-      input: 'src',
-      output: 'dist'
+      input: "src",
+      output: "dist",
     },
-    templateFormats: [
-      'md',
-      'njk',
-      'png',
-      'jpg',
-      'svg',
-      'woff',
-      'woff2'
-    ],
-    passthroughFileCopy: true
-  }
-}
+    templateFormats: ["md", "njk", "png", "jpg", "svg", "woff", "woff2"],
+    passthroughFileCopy: true,
+  };
+};
